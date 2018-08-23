9926cbf03ad0b526c3e26d1433ebdbd2d7098c5e
Juan Moreno <juan.andres.208@hotmail.com>
2018-08-23 16:20:18 -0500

    parte 1

--- angular-seed/src/app/services/todo.service.ts
+++ angular-seed/src/app/services/todo.service.ts
@@ -16,4 +16,12 @@ export class TodoService {
     return this.todos;
   }
 
+  create(a: string, b: Number, c: Boolean) {
+    if (a == null || b == null || c == null) {
+      console.error('Some of TODOs parameters is null');
+      return;
+    }
+    this.todos.push(new Todo(a, b, c.valueOf()));
+  }
+
 }
